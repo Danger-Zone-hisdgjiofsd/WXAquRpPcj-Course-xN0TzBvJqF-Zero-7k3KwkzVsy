717f53b88ec86e990aca9a6fb9ce5fbcf52ca42d
tom <hkcsser@gmail.com>
2019-01-31 02:24:27 +0800

    Initial

---
+++ CourseZero/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CourseZero
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            var host = CreateWebHostBuilder(args).Build();
+            Console.WriteLine("Run webhost");
+            host.Run();
+            Console.WriteLine("End webhost");
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
