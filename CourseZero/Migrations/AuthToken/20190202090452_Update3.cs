f1139d7b9dd57bba26e459af683d275808d63e47
tom <hkcsser@gmail.com>
2019-02-02 17:06:46 +0800

    created auth hash database

---
+++ CourseZero/Migrations/AuthToken/20190202090452_Update3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseZero.Migrations.AuthToken
+{
+    public partial class Update3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuthToken",
+                columns: table => new
+                {
+                    Token = table.Column<string>(nullable: false),
+                    userID = table.Column<int>(nullable: false),
+                    Last_access_IP = table.Column<string>(nullable: true),
+                    Last_access_Location = table.Column<string>(nullable: true),
+                    Last_access_Time = table.Column<DateTime>(nullable: false),
+                    Last_access_Device = table.Column<string>(nullable: true),
+                    Last_access_Browser = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthToken", x => x.Token);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthToken");
+        }
+    }
+}
