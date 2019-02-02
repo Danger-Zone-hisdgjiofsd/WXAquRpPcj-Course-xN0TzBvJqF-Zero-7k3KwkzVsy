cf974079fdc2f269b93bbd2af6e7bb308aae118d
tom <hkcsser@gmail.com>
2019-02-02 17:08:54 +0800

    Renamed folder. Added RequestSource Tool.

---
+++ CourseZero/Tools/Hashing_Tool.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace CourseZero.Hashing
+{
+    public class Hashing_Tool
+    {
+        public static (string salt, string hashed_pw) Hash_Password_by_Random_Salt(string password)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 1000,
+                numBytesRequested: 256 / 8));
+            return (Convert.ToBase64String(salt), hashed);
+        }
+        public static string Hash_Password(string password, string salt_in_base64)
+        {
+            byte[] salt = Convert.FromBase64String(salt_in_base64);
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 1000,
+                numBytesRequested: 256 / 8));
+            return hashed;
+        }
+        public static string Random_String(int length)
+        {
+            if (length * 3 % 4 != 0)
+                throw new Exception("length has to be divisible by 0.75");
+            byte[] result = new byte[(int)(length*0.75)];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(result);
+            }
+            return Convert.ToBase64String(result);
+        }
+    }
+}
---
+++ CourseZero/Tools/RequestSource_Tool.cs
@@ -0,0 +1,42 @@
+﻿using CourseZero.Models;
+using DeviceDetectorNET;
+using DeviceDetectorNET.Cache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CourseZero.Tools
+{
+    public class RequestSource_Tool
+    {
+        public static DeviceDetector deviceDetector = new DeviceDetector();
+        static WebClient wc = new WebClient();
+        public static (string operation_system, string browser) Decode_UA(string ua)
+        {
+            deviceDetector.SetUserAgent(ua);
+            deviceDetector.Parse();
+            string operation_system = "unknown";
+            string browser = "unknown";
+            var os_info = deviceDetector.GetOs();
+            if (os_info.Success)
+                operation_system = os_info.Match.Name + " " + os_info.Match.Platform + " " + os_info.Match.Version;
+            var browser_info = deviceDetector.GetBrowserClient();
+            if (browser_info.Success)
+                browser = browser_info.Match.Name + " " + browser_info.Match.Version;
+            return (operation_system, browser);
+        }
+        public async static Task<string> IP_to_Location(string ip)
+        {
+            try
+            {
+                return await wc.DownloadStringTaskAsync(new Uri("https://ipapi.co/"+ip+"/country_name"));
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
