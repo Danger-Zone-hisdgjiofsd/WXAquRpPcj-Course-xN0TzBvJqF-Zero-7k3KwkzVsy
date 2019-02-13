e3ae25d22949ce493161a5c4e8b5897ec620c5c7
tom <hkcsser@gmail.com>
2019-02-14 02:43:08 +0800

    initial, paging logic in js

---
+++ CourseZero/wwwroot/js/paging.js
@@ -0,0 +1,34 @@
+function Paging_loadMain()
+{
+	//To be done
+	Paging_load("./test_pagetoload.html");
+}
+function Paging_loadLogin()
+{
+	Paging_load("./testlogin.html");
+}
+function Paging_load(path)
+{
+	Paging_ShowSpinner(true);
+	$("#Layout_frame_content").load(path, function() {
+		Paging_ShowSpinner(false);
+	});
+}
+function Paging_ShowSpinner(show)
+{
+	if (show)
+	{
+		$("#Layout_spinner_layer").show();
+		$("#Layout_frame_content").prop('disabled', true);
+	}
+	else
+	{
+		$("#Layout_spinner_layer").hide();
+		$("#Layout_frame_content").prop('disabled', false);
+	}
+}
+$(document).ready(function () {
+	Paging_loadMain();
+	$("#Layout_loginbtn").click(Paging_loadLogin);
+	$("#Layout_homebtn").click(Paging_loadMain);
+});
