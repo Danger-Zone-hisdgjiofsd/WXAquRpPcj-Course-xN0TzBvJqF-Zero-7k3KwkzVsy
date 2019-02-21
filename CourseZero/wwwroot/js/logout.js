bb69e519689e480d7d97e0d695909e4557bc4d72
Tsz Yin WONG <1155093245@link.cuhk.edu.hk>
2019-02-21 10:36:27 +0800

    idk

--- CourseZero/wwwroot/js/logout.js
+++ CourseZero/wwwroot/js/logout.js
@@ -30,8 +30,13 @@ function adddiv(id) {
     element.id = "Logout_Session_frame_" + id;
     element.setAttribute("onclick", "pickLogout(this);");
     var word = document.createElement('div');
-    word.className = "col-8";
-
+    word.className = "col-6";
+    var picframe = document.createElement('div');
+    var pic = document.createElement('img');
+    picframe.className = "col-3"
+    pic.setAttribute("src", "./material/chrome.png");
+    pic.setAttribute("style", "width:50px;height:50px;");
+    pic.id = "Logout_Session_Pic_" + id;
     var LAT = document.createElement('p');
     LAT.id = "Logout_Session_LAT_" + id;
     LAT.innerHTML = "Last Access Time:";
@@ -44,28 +49,37 @@ function adddiv(id) {
     word.appendChild(LAT);
     word.appendChild(LAD);
     word.appendChild(LAB);
-
+    picframe.appendChild(pic);
     element.appendChild(word);
-
+    element.appendChild(picframe);
     document.getElementsByClassName("container-fluid no-padding")[0].appendChild(element);
 
 }
 
-var sessions;
+var sessions,session_left;
+function checkShow() {
+    if (session_left > 1)
+        document.getElementById("Logout_Session_btn").style.display = "block";
+    else document.getElementById("Logout_Session_btn").style.display = "none";
+}
 function getAllSession() {
     var msg_to_send = {
         "auth_token": null
     }
-    console.log(g_auth_token);
-    
+  
     msg_to_send.auth_token = g_auth_token;
-    console.log(msg_to_send);
+
     postJSON("/api/Session/Get_All_Sessions", msg_to_send, function (obj) {
         sessions = obj["sessions"];
         if (obj["status_code"] == 0) //success
         {
+            session_left = sessions.length;
+            console.log(sessions.length);
+            console.log(session_left);
+            checkShow();
             
             var text;
+            var browser = ["chrome", "edge", "internet explorer", "firefox", "sarafi", "opera"];
             for (var i = 0; i < sessions.length; i++) {
                 if (sessions[i].token == msg_to_send.auth_token) {
                     text = document.getElementById("Logout_Session_Current_device");
@@ -82,6 +96,18 @@ function getAllSession() {
                     text.innerHTML = "Last Access Device: " + sessions[i].last_access_Device;
                     text = document.getElementById("Logout_Session_LAB_" + i);
                     text.innerHTML = "Last Access Browser: " + sessions[i].last_access_Browser;
+                    text = document.getElementById("Logout_Session_Pic_" + i);
+                    
+
+              
+
+                }
+                for (var j = 0; j < browser.length; j++) {
+                    if (sessions[i].last_access_Browser.toLowerCase().includes(browser[j])) {
+
+                        var output = "./material/" + browser[j] + ".png";
+                        text.setAttribute("src", output);
+                    }
                 }
             }
         }
@@ -94,6 +120,8 @@ function pickLogout(obj) {
         var logout_select = id.substring(21);
         Logout_Spec(sessions[logout_select].token);
         obj.remove();
+        session_left--;
+        checkShow();
         alert("Done");
     }
 }
@@ -103,9 +131,20 @@ function SignOut_Clicked()  {
         if (confirm('Logout?')) {
             Logout();
             Paging_loadMain();
-            $("#layout_avatar").hide();
+            $("#dropdown_avatar").hide();
             $("#Layout_loginbtn").show();
 
         }
     }
 }
+
+function logout_All_Session() {
+    for (var i = 0; i < sessions.length; i++) {
+        if (sessions[i].token != g_auth_token) {
+            Logout_Spec(sessions[i].token);
+            var remo = "#Logout_Session_frame_" + i;
+            $(remo).remove();
+        }
+    }
+}
+
