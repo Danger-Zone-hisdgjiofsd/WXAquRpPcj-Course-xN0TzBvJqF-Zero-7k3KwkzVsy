d89316dfd622e57a6b0167dee529aaf99882679a
tom <hkcsser@gmail.com>
2019-03-04 01:52:44 +0800

    added 2 model, filecomment, rating

---
+++ CourseZero/Models/FileComment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseZero.Models
+{
+    public class FileComment
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public int sender_UserID { get; set; }
+        public int file_ID { get; set; }
+        [MaxLength(2048)]
+        public string Text { get; set; }
+        public DateTime posted_dateTime { get; set; }
+    }
+    public class FileComment_ShownToUser
+    {
+        public int ID { get; set; }
+        public int sender_UserID { get; set; }
+        public string Text { get; set; }
+        public DateTime posted_dateTime { get; set; }
+        public string sender_Username { get; set; }
+
+    }
+}
+
